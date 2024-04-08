8fea8012a5f8cde2b23e7148c27afb2ffc45a180
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2024-04-07 19:40:48 -0600

    revisiojn

--- redimenzionar.py
+++ redimenzionar.py
@@ -2,13 +2,14 @@ import os
 import cv2
 
 directorio_imagenes = './datasets/original-dataset/test'
-
+directorio_imagenes_modificadas = './datasets/modified-dataset/test'
 roi_x, roi_y, roi_width, roi_height = 400, 400, 600, 600
+salmofan = ""
 
 for nombre_archivo in os.listdir(directorio_imagenes):
     if nombre_archivo.endswith(".jpg"):
         ruta_imagen = os.path.join(directorio_imagenes, nombre_archivo)
         imagen = cv2.imread(ruta_imagen)
         roi = imagen[roi_y:roi_y+roi_height, roi_x:roi_x+roi_width]
-        cv2.imwrite(ruta_imagen, roi)
+        cv2.imwrite(directorio_imagenes_modificadas, roi)
 print("Proceso completado.")
